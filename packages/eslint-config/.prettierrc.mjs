326eda160989c32577ed3c5e74c31692fc53a85a
行走的麦芽糖 <modai888@hotmail.com>
2024-04-17 16:18:43 +0800

    release: update version to 0.1.8

--- packages/eslint-config/.prettierrc.mjs
+++ packages/eslint-config/.prettierrc.mjs
@@ -1,4 +1,4 @@
 // 配置本地prettier格式化选项
 // 支持VSCode Shift + Alt + F快速格式化命令
-import config from '@shareable-scripts/common-scripts/lib/config/prettier.cjs';
+import config from '../common-scripts/src/config/prettier.cjs';
 export default config;
